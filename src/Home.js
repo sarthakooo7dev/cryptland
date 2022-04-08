a24c57ba53917652690986f47e3b7fad9e950257
Sarthak Shukla <sarthak.shukla007@gmail.com>
2022-04-08 16:03:22 +0530

    video inbuild with navbar commit -3

--- src/Home.js
+++ src/Home.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d9c1a03ab387923686eed8cb8b2b657b7a7f24777b4b69509263c44e28499573
-size 281
+oid sha256:9a99f1cfede0e08923433e8a5413787f49e6e0ee2681a55f6e1e4d4e3beb9023
+size 729
