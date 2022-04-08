78f2049a36f659d2a58728a65b4017d9afbce586
Sarthak Shukla <sarthak.shukla007@gmail.com>
2022-04-08 14:32:53 +0530

    commit -2 basic with navbar

--- src/Home.js
+++ src/Home.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f53f063da28b29575d1e4df5e54b6235f1e19b709606d84baaf4aa1a83eb169
-size 1627
+oid sha256:d9c1a03ab387923686eed8cb8b2b657b7a7f24777b4b69509263c44e28499573
+size 281
